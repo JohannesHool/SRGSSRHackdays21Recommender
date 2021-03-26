cb404fa960b2c87faf8fd891a3f72b68f50f07f4
JohannesHool <hooljohannes@gmail.com>
2021-03-26 10:11:51 +0100

    Added endpoint which expects a json and returns a list of recommender styles

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 articles = pd.read_csv('data/articles.csv')
 
 @app.route('/')
-
 def hello_world():
     return 'This is the index page... nothing going on...'
 
@@ -29,6 +28,39 @@ def get_author_recommendations():
         return authorRecommender.get_recommendations(articles, ids, ratings)
     except Exception as e:
         return str(e)
+
+
+@app.route('/recommend', methods=['GET'])
+def get_recommendations_from_json():
+
+    content = request.json
+    ids = []
+    ratings = []
+
+    for rating in content['ratings']:
+        ids.push(int(rating['id']))
+        ratings.push(int(rating['rating']))
+
+    recommendations = []
+
+    author_recommendations = authorRecommender.get_recommendations(articles, ids, ratings)
+    recommendations.append({'name': author_recommendations['name'], 'recommendations': author_recommendations['articles'], 'reason': author_recommendations['reason'], 'certainty': author_recommendations['certainty']})
+
+    return jsonify(results=recommendations)
+
+
+@app.route('/recommendwojson', methods=['GET'])
+def get_recommendations():
+
+    recommendations = []
+    ids = [int(id) for id in request.args.get('ids').split(',')]
+    ratings = [int(rating) for rating in request.args.get('ratings').split(',')]
+
+    author_recommendations = authorRecommender.get_recommendations(articles, ids, ratings)
+
+    recommendations.append({'name': author_recommendations['name'], 'recommendations': author_recommendations['articles'], 'reason': author_recommendations['reason'], 'certainty': author_recommendations['certainty']})
+
+    return jsonify(results=recommendations)
 
 
 if __name__ == '__main__':
